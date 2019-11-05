2bbae4ee743776b0f0c64a5c69ef957abd934d73
ProtikHore <protik.hore@gmail.com>
2019-11-05 22:17:06 +0600

    Functioanl Components

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Greet() {
+    return <h2>Hello Protik</h2>
+}
+
+//const Greet = () => <h2>Hello Protik</h2>
+//export const Greet = () => <h2>Hello Protik</h2> // named export
+
+export default Greet
